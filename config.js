d9251b93500da24cea6c90c62b9a91847e2a36f1
Evanwoon1z <evanwoon1z@gmail.com>
2025-04-18 14:51:05 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+  ownerId: '123456789012345678',
+  status: {
+    rotateDefault: [
+      { name: 'the server', type: ActivityType.Watching },
+    ],
+    songStatus: true
+  },
+  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
+}
